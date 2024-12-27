a36b8ac0657850a2d9467250c1b2378cf30cf253
Anthony Rappa <rappa819@gmail.com>
2024-12-27 15:44:11 -0500

    Add missing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Spatie\Health\Commands\RunHealthChecksCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(RunHealthChecksCommand::class)->everyMinute();
+        $schedule->command('backup:clean')->daily()->at('01:00')->timezone('America/New_York');
+        $schedule->command('backup:run')->daily()->at('01:30')->timezone('America/New_York');
     }
 
     /**
