a0836f9733469a442e5e80f7559cca8cbe8cde34
Anthony Rappa <rappa819@gmail.com>
2025-01-08 14:42:37 -0500

    Updates

--- resources/views/livewire/client/passive/cd-bonds/index.blade.php
+++ resources/views/livewire/client/passive/cd-bonds/index.blade.php
@@ -5,6 +5,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl space-y-6 mx-auto sm:px-6 lg:px-8">
+            <x-alerts.info>
+                <p>Certificates of Deposit (CDs) and bonds are excellent options for generating passive income due to their stability and predictable returns. CDs offer a fixed interest rate over a set term, ensuring your money grows consistently without market volatility. They are also typically insured by the FDIC, providing a secure investment vehicle for short- or medium-term goals. Bonds, whether issued by governments or corporations, offer regular interest payments and the return of principal upon maturity, making them a reliable source of income. While bonds can vary in risk depending on the issuer, high-quality bonds, like those from governments or blue-chip companies, are generally considered safe investments. Both CDs and bonds allow you to earn passive income without active management, making them attractive options for risk-averse investors seeking steady growth.</p>
+            </x-alerts.info>
+
             <div class="p-6 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 {{-- TODO: CD/Bond affiliate links --}}
 
--- resources/views/livewire/client/passive/dividends/index.blade.php
+++ resources/views/livewire/client/passive/dividends/index.blade.php
@@ -5,6 +5,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl space-y-6 mx-auto sm:px-6 lg:px-8">
+            <x-alerts.info>
+                <p>Dividend stocks are an excellent option for generating passive income because they provide regular payouts in addition to the potential for capital appreciation. These stocks represent shares of companies that distribute a portion of their earnings to shareholders, typically on a quarterly basis. Dividend-paying companies are often well-established and financially stable, making them a relatively reliable source of income. Unlike fixed-income investments like CDs or bonds, dividend stocks also offer the opportunity for your initial investment to grow in value over time, allowing you to build wealth while earning passive income. By reinvesting dividends, you can further accelerate your portfolio’s growth through the power of compounding. This combination of steady income and potential long-term gains makes dividend stocks a versatile and attractive option for passive income seekers.</p>
+            </x-alerts.info>
+
             <div class="p-6 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 {{-- TODO: Dividends affiliate links --}}
 
